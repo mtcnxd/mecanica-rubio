8cdcd97c3f628331387608dfe8ad88a2606825cc
Marcos Tzuc Cén <mtc.nxd@gmail.com>
2026-01-28 23:53:59 -0600

    Crear PayrollsService.php

---
+++ app/Services/PayrollsService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+class PayrollsService 
+{
+    public function find (string $id)
+    {
+    }
+    
+    public function update (string $id, array $data)
+    {
+        
+    }
+}
