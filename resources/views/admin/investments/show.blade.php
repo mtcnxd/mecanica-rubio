4928520e375552f3957011f7835fd0925228d2f9
Marcos Tzuc Cén <mtc.nxd@gmail.com>
2026-03-19 21:59:52 -0600

    Dates calculte with diff in days

--- resources/views/admin/investments/show.blade.php
+++ resources/views/admin/investments/show.blade.php
@@ -33,7 +33,7 @@
             </div>
             <div class="col-md-3">
                 <x-card_simple_overview_1
-                    title="% Incremento a 30 días" 
+                    title="% Incremento a {{ $first->date->diffInDays($last->date) }} días" 
                     message="{{ Number::percentage(App\Http\Helpers::convertToPercentage($first->amount, $last->amount), 2); }}"
                 />
             </div>  
